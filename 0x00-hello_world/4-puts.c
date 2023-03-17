2b7a6a0a0325ea59ffbb00522ed2490d70358437
Nacer-eddine1 <snasro58gmail.com>
2023-03-16 17:37:20 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+ *  main - Entry point
+ *
+ */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	
+/* 
+ * Return: Always 0 (Success)
+ */
+	return(0); 
+	}
